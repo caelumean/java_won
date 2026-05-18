0d37c7b12d8b3a3e099c55851b1753cbc2cfd3b7
caelumean215 <98576510+caelumean@users.noreply.github.com>
2026-05-18 15:33:57 +0900

    [Update]

---
+++ workspace/ch05/ex01/PrimitiveType.java
@@ -0,0 +1,14 @@
+package ch05.ex01;
+
+public class PrimitiveType
+{
+    void main()
+    {
+        // 변수 : 데이터를 담는 곳.
+        //       값을 할당해야한다.
+        int age;    // 변수 선언
+        age = 25;   // 변수에 값 할당
+
+        System.out.println(age);
+    }
+}
